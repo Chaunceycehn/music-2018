a7a11150e2ff12cd952c18a11a94bd10086e6a16
chauncey <chchuang@foxmail.com>
2018-09-10 15:33:59 +0800

    获取歌曲列表和点击激活

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -1,48 +1,79 @@
 {
     let view = {
-        el : '#songList-container',
-        template : `
+        el: '#songList-container',
+        template: `
         <ul class="songList">
         </ul>
         `,
-        render(data){
+        render(data) {
             let $el = $(this.el)
             $el.html(this.template)
-            let {songs} = data
-            let liList = songs.map((song)=> {
+            let { songs } = data
+            let liList = songs.map((song) => {
                 let $li = $('<li></li>').text(song.name)
                 return $li
             })
             $el.find('ul').empty()
-            liList.map((domLi)=>{
+            liList.map((domLi) => {
                 $el.find('ul').append(domLi)
             })
         },
-        clearActive(){
+        activeItem(li){
+            let $li = $(li)
+            $li.addClass('active')
+                .siblings('.active').removeClass('active')
+        },
+        clearActive() {
             $(this.el).find('.active').removeClass('active')
         }
-    }  
+    }
     let model = {
-        data:{
-            songs:[
+        data: {
+            songs: [
 
             ]
+        },
+        find(){
+            var query = new AV.Query('Song');
+            return query.find().then((songs) => {
+                this.data.songs = songs.map((song) => {
+                    return {id:song.id, ...song.attributes}
+                })
+                return this.data.songs
+            })
         }
     }
     let controller = {
-        init(view,model){
+        init(view, model) {
             this.view = view
             this.model = model
+            this.bindEvents()
+            this.bindEventhub()
+            this.getallsongs()
             this.view.render(this.model.data)
-            window.eventHub.on('upload',() => {
+
+        },
+        getallsongs(){
+            return this.model.find().then(() => {
+                this.view.render(this.model.data)
+            })
+        },
+        bindEvents(){
+            $(this.view.el).on('click','li',(e)=>{
+                this.view.activeItem(e.currentTarget)
+            })
+        },
+        bindEventhub(){
+            window.eventHub.on('upload', () => {
                 this.view.clearActive()
             })
-            window.eventHub.on('create',(songData) => {
+            window.eventHub.on('create', (songData) => {
                 this.model.data.songs.push(songData)
                 this.view.render(this.model.data)
 
             })
         }
-    } 
-    controller.init(view,model)
+
+    }
+    controller.init(view, model)
 }
